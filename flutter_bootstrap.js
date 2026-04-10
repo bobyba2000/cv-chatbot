fa239b632b6212583f934d123afc901457f03c3f
bobyba2000 <62866371+bobyba2000@users.noreply.github.com>
2026-04-10 10:12:00 +0000

    Deploying to gh-pages from @ bobyba2000/cv-chatbot@148676f525ebc95ba324cadcc85bc97233881d87 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3053951413"
+    serviceWorkerVersion: "1113217394"
   }
 });
